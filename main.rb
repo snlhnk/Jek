18bf7ee44251df7baa9f49203bcd9df5d4a79db8
Mamiya ENT Clinic <mamiya.ent@gmail.com>
2015-12-15 02:23:21 +0900

    get green to showing uploaded data spec

--- main.rb
+++ main.rb
@@ -5,11 +5,26 @@ get "/" do
   erb :index
 end
 
+post "/proc" do
+  unless params[:file] &&
+      (tmpfile = params[:file][:tempfile]) &&
+      (name = params[:file][:filename])
+    @error = "No file selected"
+  end
+
+  tmpfile.open do |t|
+    p t.gets
+  end
+end
+
 __END__
 
 @@ index
 <html>
 <body>
-Hello, world
+  <form action="/proc" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file" />
+    <input type="submit" value="process" />
+  </form>
 </body>
 </html>
